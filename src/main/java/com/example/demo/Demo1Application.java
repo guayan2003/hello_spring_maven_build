b6c477e1d0f41a110d17ac95980c44eb4088d287
m2730 <m2730@Frankie-Zheng.wanhai.com>
2021-04-21 09:55:24 +0800

    Update POM (add dependency)

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Demo1Application {
+public class Demo1Application extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Demo1Application.class);
+	}
+	
 	public static void main(String[] args) {
-		SpringApplication.run(Demo1Application.class, args);
+        SpringApplication app = new SpringApplication(Demo1Application.class);
+//        app.setDefaultProperties(Collections
+//          .singletonMap("server.port", "8150"));
+        app.run(args);		
 	}
 
 }
